7b23bca89ab626ef671a6ba4b1bb9615dce8ea0c
saivineesh995 <saivineeshtippireddy@gmail.com>
2025-04-11 19:44:25 -0500

    Added index.html

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [students, setStudents] = useState([]);
 
   useEffect(() => {
-    fetch('https://clouddevbackend-e9f7g2h3hdhrbpde.centralus-01.azurewebsites.net/api/students')
+    fetch('https://clouddevelopmentwebapphw2-grcfbjcagvhhc8bu.canadacentral-01.azurewebsites.net/api/students')
       .then(res => res.json())
       .then(data => setStudents(data))
       .catch(err => console.error('Error fetching students:', err));
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Student List</h1>
+      <h1>Student List for vineesh</h1>
       <table border="1" cellPadding="10">
         <thead>
           <tr>
